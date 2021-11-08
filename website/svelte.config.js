a2d1c96b9e3dc7f33bdedfd9a1fabd84aa842b2a
radva <radoje.vasovic01@gmail.com>
2021-11-08 16:25:10 +0100

    vercel adapter

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
