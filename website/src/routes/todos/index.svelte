82ac5aaa8208a5d2b8df22be1b7a264c28543ff0
radva <radoje.vasovic01@gmail.com>
2021-11-09 14:02:47 +0100

    removed form because build was breaking

--- website/src/routes/todos/index.svelte
+++ website/src/routes/todos/index.svelte
@@ -1,24 +1,5 @@
 <script context="module">
-	import { enhance } from '$lib/form';
 
-	// see https://kit.svelte.dev/docs#loading
-	export const load = async ({ fetch }) => {
-		const res = await fetch('/todos.json');
-
-		if (res.ok) {
-			const todos = await res.json();
-
-			return {
-				props: { todos }
-			};
-		}
-
-		const { message } = await res.json();
-
-		return {
-			error: new Error(message)
-		};
-	};
 </script>
 
 <script>
