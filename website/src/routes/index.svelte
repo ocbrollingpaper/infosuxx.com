a2caa89f3607ee6029d1da3d93395eee408d562d
radva <radoje.vasovic01@gmail.com>
2021-11-10 11:59:35 +0100

    remove logos

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -8,12 +8,6 @@
 </svelte:head>
 
 <section>
-	<div class="logos">
-		<a href="https://threatfox.abuse.ch/" target="_blank"><img src="/threatfox_logo.png" alt="ThreatFox by abuse.ch"></a>
-		<a href="https://vx-underground.org/" target="_blank"><img src="/vxunderground_logo.png" alt="VX Undergroundg"></a>
-		<a href="https://greynoise.io/" target="_blank"><img src="/greynoise_logo.png" alt="Greynoise.io"></a>
-		<a href="https://tria.ge/" target="_blank"><img src="/triage_logo.png" alt="Tria.ge"></a>
-	</div>
 </section>
 
 <style>
@@ -23,26 +17,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-	h1 {
-		width: 100%;
-		font-family: 'Oswald';
-	}
-	h2 {
-		width: 100%;
-		font-family: 'Oswald';
-	}
-	.logos {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-		height: 4rem;
-		overflow: hidden;
-		max-height: 100px;
-	}
-	.logos img {
-		margin: 1rem;
 	}
 </style>
